05c1509c0449d5f777a6ee63e6935b4c7912365a
ismailherianto <ismail@penjaminan-online.id>
2025-03-21 09:36:23 +0800

    update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-      "./resources/js/**/*.jsx",
-      "./features/**/*.jsx", // Komponen React
+      "./resources/js/**/*.tsx",
+      "./features/**/*.tsx", // Komponen React
       "./resources/views/**/*.blade.php", // File Blade Laravel
     ],
     
